6482a201b99815fc0d2ad4ba2c7d3d6f83128618
OSWALDO DA SILVA BARBOSA DOS SANTOS <22282212@CFP127ALUNO.LOCAL>
2023-03-06 16:43:53 -0300

    aula08

--- aula08/modulo/exercicioJsonArray.js
+++ aula08/modulo/exercicioJsonArray.js
@@ -29,31 +29,151 @@ const getListaDeEstados = function () {
     }
 }
 
-// console.log(getListaDeEstados())
+const getDadosEstados = function (estadoSigla) {
 
-const getDadosEstados = function(estadoSigla) {
-    //Entrada de Dados
-    let siglaEstado = estadoSigla 
-    let listaDeEstadosArrayUf = []
-    let listaDeEstadosArrayDescricao = []
-    let listaDeEstadosArrayCapital = []
-    let listaDeEstadosArrayRegiao = []
+    if (estadoSigla == "") {
+        return false
+    } else {
 
+        //Entrada de Dados
+        let siglaEstado = estadoSigla.toUpperCase()
+        let listaInformacoesDosEstados
+        let status
 
-    //Processamento da Função
-    varEstadosCidades.estadosCidades.estados.forEach(function(estado) {
-        listaDeEstadosArrayUf.push(estado.sigla)
-        listaDeEstadosArrayDescricao.push(estado.nome)
-        listaDeEstadosArrayCapital.push(estado.capital)
-        listaDeEstadosArrayRegiao.push(estado.regiao)
-    })
+        //Processamento da Função
+        varEstadosCidades.estadosCidades.estados.forEach(function (estado) {
+            if (estado.sigla == siglaEstado) {
+                listaInformacoesDosEstados = {
+                    uf: estado.sigla,
+                    descricao: estado.nome,
+                    capital: estado.capital,
+                    regiao: estado.regiao
+                }
+                status = true
+            }
+        })
 
-    let listaDeEstadosJson = { uf: listaDeEstadosArrayUf, descricao: listaDeEstadosArrayDescricao, capital: listaDeEstadosArrayCapital, regiao: listaDeEstadosArrayRegiao }
-
-    siglaEstado = listaDeEstadosJson
-
-    return siglaEstado
-
+        //Saída
+        if (status == true) {
+            return listaInformacoesDosEstados
+        } else {
+            return false
+        }
+    }
 }
 
-console.log(getDadosEstados("SP"))
+const getCapitalEstado = function (estadoSigla) {
+
+    if (estadoSigla == "") {
+        return false
+    } else {
+
+        //Entrada de Dados
+        let siglaEstado = estadoSigla.toUpperCase()
+        let listaInformacoesDosEstados
+        let status
+
+        //Processamento da Função
+        varEstadosCidades.estadosCidades.estados.forEach(function (estado) {
+            if (estado.sigla == siglaEstado) {
+                listaInformacoesDosEstados = {
+                    uf: estado.sigla,
+                    descricao: estado.nome,
+                    capital: estado.capital
+                }
+                status = true
+            }
+        })
+
+        if (status) {
+            return listaInformacoesDosEstados
+        } else {
+            return false
+        }
+    }
+}
+
+const getEstadosRegiao = function (regiaoEstado) {
+    //Entrada de Dados
+    let regiao = regiaoEstado
+    let listaDeRegiaoJSON = {}
+    let listaDeEstadosARRAY = []
+    let status
+
+    //Processamento da Função
+    varEstadosCidades.estadosCidades.estados.forEach(function (estado) {
+        if (estado.regiao == regiao) {
+            listaDeRegiaoJSON.regiao = estado.regiao
+            let listaDeEstadoJSON = {}
+            listaDeEstadoJSON.uf = estado.sigla
+            listaDeEstadoJSON.descricao = estado.nome
+            listaDeEstadosARRAY.push(listaDeEstadoJSON)
+        }
+        status = true
+    })
+
+    //Saída
+    if (status) {
+        listaDeRegiaoJSON.estados = listaDeEstadosARRAY
+        return listaDeRegiaoJSON
+    } else {
+        return false
+    }
+}
+
+const getCapitalPais = function () {
+    //Entrada de Dados
+    let listaDeCapitalJSON = {}
+    let listaDeCapitalARRAY = []
+
+    //Processamento da Função
+    varEstadosCidades.estadosCidades.estados.forEach(function (estado) {
+        let listaInformacoesDasCidadesJSON = {}
+        if (estado.capital_pais != undefined) {
+            listaInformacoesDasCidadesJSON.capital_atual = estado.capital_pais.capital
+            listaInformacoesDasCidadesJSON.uf = estado.sigla
+            listaInformacoesDasCidadesJSON.descricao = estado.nome
+            listaInformacoesDasCidadesJSON.capital = estado.capital
+            listaInformacoesDasCidadesJSON.capital_pais_ano_inicio = estado.capital_pais.ano_inicio
+            listaInformacoesDasCidadesJSON.capital_pais_ano_fim = estado.capital_pais.ano_fim
+            listaDeCapitalARRAY.push(listaInformacoesDasCidadesJSON)
+        }
+        status = true
+    })
+
+    //Saída
+    if (status) {
+        return listaDeCapitalJSON.capitais = listaDeCapitalARRAY
+    } else {
+        return false
+    }
+}
+
+const getCidades = function (estadoSigla) {
+    //Entrada de Dados
+    let siglaEstado = estadoSigla
+    let listaInformacoesCidadesJSON = {}
+    let listaDeCidadesARRAY = []
+    let status
+
+    //Processamneto da Função
+    varEstadosCidades.estadosCidades.estados.forEach(function (estado) {
+        if (estado.sigla == siglaEstado) {
+            listaInformacoesCidadesJSON.uf = estado.sigla
+            listaInformacoesCidadesJSON.descricao = estado.nome
+            listaInformacoesCidadesJSON.quantidade_cidades = estado.cidades.length
+            estado.cidades.forEach(function (estadoCidade) {
+                listaDeCidadesARRAY.push(estadoCidade.nome)
+            })
+            listaInformacoesCidadesJSON.cidades = listaDeCidadesARRAY
+        }
+        status = true
+    })
+
+    //Saída
+    if (status) {
+        return listaInformacoesCidadesJSON
+    } else {
+        return false
+    }
+}
